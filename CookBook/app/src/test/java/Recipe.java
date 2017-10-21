ffdcff9401002b715acef43a9cc094ade69471ce
Joshua Bump <joshuabump@joshuab-mbp.student.iastate.edu>
2017-10-21 00:49:13 -0500

    added chosenIngredient

--- CookBook/app/src/test/java/Recipe.java
+++ CookBook/app/src/test/java/Recipe.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -11,4 +12,53 @@ public class Recipe {
     private String cookingTime;
     private String recipeName;
     private ArrayList<String> ingredients;
-}
+    private int servingSize;
+
+
+
+    public Recipe(String cTime, String rName, ArrayList<String> ing, int sSize) {
+        cookingTime=cTime;
+        recipeName=rName;
+        ingredients=ing;
+        servingSize=sSize;
+
+
+    }
+
+    public String getRecipeName() {
+
+        return recipeName;
+    }
+
+    public String getCookingTime() {
+        return cookingTime;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public int getServingSize() {
+        return servingSize;
+    }
+
+    public boolean containsIngredient() {
+
+        new scanner = Scanner(System.in);
+
+        String chosenIngredient;
+
+        int arrayLength=ingredients.length();
+
+        for(int i=0;i<arrayLength;i++) {
+
+            if (ingredients(i)==chosenIngredient) {
+
+                return true;
+            }
+
+            }
+        }
+
+    }
+
