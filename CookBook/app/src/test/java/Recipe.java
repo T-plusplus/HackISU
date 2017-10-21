4a0a369eba50014ac5ae906ec37e45073cafa38e
Joshua Bump <joshuabump@joshuab-mbp.student.iastate.edu>
2017-10-20 23:34:40 -0500

    adding recipe

---
+++ CookBook/app/src/test/java/Recipe.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by joshuabump on 10/20/17.
+ */
+
+public class Recipe {
+
+
+
+    private String cookingTime;
+    private String recipeName;
+    private ArrayList<String> ingredients;
+}
