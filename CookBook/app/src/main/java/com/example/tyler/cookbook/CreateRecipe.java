95be40c73a2d41fa3d6e122d2517e314212e816a
bedert <bedert@iastate.edu>
2017-10-21 14:16:34 -0500

    starting volley

---
+++ CookBook/app/src/main/java/com/example/tyler/cookbook/CreateRecipe.java
@@ -0,0 +1,13 @@
+package com.example.tyler.cookbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe);
+    }
+}
---
+++ CookBook/app/src/main/java/com/example/tyler/cookbook/apiURL.java
@@ -0,0 +1,15 @@
+package com.example.tyler.cookbook;
+
+/**
+ * Created by Tyler on 10/21/2017.
+ */
+
+public class apiURL {
+    public static final String url = "https://api.edamam.com";
+    public static final String searchpath="https://api.edamam.com/search";
+
+    public static final String appID="32644bcd";
+    public static final String appKey="f70207a7b8a0188cfa5f49caaaa19fee";
+
+
+}
