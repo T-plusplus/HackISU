dc52c3419866de2b7ff38dc7607a0e27d06849ec
bedert <bedert@iastate.edu>
2017-10-21 20:33:57 -0500

    made better

--- CookBook/app/src/main/java/com/example/tyler/cookbook/CreateRecipe.java
+++ CookBook/app/src/main/java/com/example/tyler/cookbook/CreateRecipe.java
@@ -1,5 +1,6 @@
 package com.example.tyler.cookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -23,12 +24,19 @@ EditText RN,Ing,SS,CT,DIR;
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(RN.getText().toString()!="") {
-                    if(Ing.getText().toString()!="") {
-                        if(SS.getText().toString()!="") {
-                            if(CT.getText().toString()!="") {
-                                if(DIR.getText().toString()!="") {
+                if(RN.getText().toString().isEmpty()) {
+                    if(Ing.getText().toString().isEmpty()) {
+                        if(SS.getText().toString().isEmpty()) {
+                            if(CT.getText().toString().isEmpty()) {
+                                if(DIR.getText().toString().isEmpty()) {
                                     //It took me so long to do the nested if elses, i literally forgot what goes here.
+
+                                    //copy to text file database
+
+                                    //start new display activity of that one
+                                    Intent i=new Intent(CreateRecipe.this, RecipeActivity.class);
+                                    startActivity(i);
+                                    finish();
                                 }
                                 else {
                                     Toast.makeText(CreateRecipe.this,"Must have Cooking Directions",Toast.LENGTH_LONG).show();
