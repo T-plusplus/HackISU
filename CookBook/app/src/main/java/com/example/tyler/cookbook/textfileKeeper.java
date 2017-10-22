d9f7a7e7b27369e4da303aa01c298d1e4ad493d8
bedert <bedert@iastate.edu>
2017-10-21 22:01:25 -0500

    adding a file with a string arg for our text file, better modular design

---
+++ CookBook/app/src/main/java/com/example/tyler/cookbook/textfileKeeper.java
@@ -0,0 +1,10 @@
+package com.example.tyler.cookbook;
+
+/**
+ * Created by Tyler on 10/21/2017.
+ */
+
+public class textfileKeeper {
+    public static final String MyRecipes="myRecipes.txt";
+    
+}
